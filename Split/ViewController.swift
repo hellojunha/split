9bbc7cee52c55bb79990ecc68fc020a679ecfd31
Alfred Woo <joona0825@gmail.com>
2019-10-11 10:19:03 +0900

    Initial Commit

---
+++ Split/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Split
+//
+//  Created by Alfred Woo on 2019/10/11.
+//  Copyright © 2019 Alfred Woo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
